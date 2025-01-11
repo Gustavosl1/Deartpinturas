bfd767210abd49f9bf5d1df58163077e5c07f112
Gustavo Lima <gusouzalima5@gmail.com>
2025-01-10 22:27:34 -0300

    Commit - Versão 1.5

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(event){
-    if(event.type === 'touchstart') event.preventDefault()
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
     const active = nav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expanded','active');
-    if(active){
+    event.currentTarget.setAttribute('aria-expanded', 'active');
+    if (active) {
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else{
+    } else {
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
     }
 
